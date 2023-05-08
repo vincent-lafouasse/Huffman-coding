a40d1106c24bd787f3f723c91c21343eab27c002
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-05-09 00:26:47 +0200

    make a 3 node tree

--- src/main.c
+++ src/main.c
@@ -4,7 +4,17 @@
 
 int main(void) {
   Node* root = new_node('\0', 0.0);
+  Node* left_leaf = new_node('6', 4.2);
+  Node* right_leaf = new_node('9', -4.2);
+  root->left = left_leaf;
+  root->right = right_leaf;
+  left_leaf->parent = root;
+  right_leaf->parent = root;
   print_node(root);
+  print_node(left_leaf);
+  print_node(right_leaf);
 
   free(root);
+  free(left_leaf);
+  free(right_leaf);
 }
